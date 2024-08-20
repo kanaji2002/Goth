fccdb88fbca7b5843a0f5a99efd18ce1b50c7ab4
kanaji2002 <gellunika6478@icloud.com>
2024-08-20 19:41:12 +0900

    タブ削除ボタンを右クリックしてもタブを削除しようとしている（のちに，関連するタブを一括削除する），現在は，まだ左右のクリックが反応しない． クローズリクエストのシグナルはインデックスのみらしい．

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,10 +1,14 @@
-<<<<<<< HEAD
-current_tab_changed
-=======
-<<<<<<< HEAD
-
-handler = Handler()
-=======
-navigate_home
->>>>>>> b0c5e73cf0ae49e450b4392723a6c4c66d12de9e
->>>>>>> 9575c06a09557504c2d6806ca91fda7ff0671e66
+        #     # スタイルシートでタブバーの高さをカスタマイズ
+        # self.tabs.setStyleSheet("""
+        #     QTabBar {
+        #         height: 50px;  /* タブバーの高さを設定 */
+        #         margin-top: 20px;  /* タブバーの上に余白を追加 */
+        #         padding : 5px;  /* タブバーの内側の余白を追加 */
+        #     }
+        #     QTabBar::tab {
+        #         margin-top: 10px;  /* タブ間の隙間を広げる */
+        #         padding: 40px;  /* タブのサイズを広げる */
+        #     }
+            
+        
+        # """)
